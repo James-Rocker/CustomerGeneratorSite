469c9bc16c223e737803f85ed6cb6f218c8de5bd
TheRockerfly <jamesrocker@hotmail.co.uk>
2020-12-27 21:39:02 +0000

    overhauled the customer generation

--- Generator/__init__.py
+++ Generator/__init__.py
@@ -1,52 +1,39 @@
 import random
 
 
-class GCN:
-    def __init__(self):
-        # Get Customer Name
-        name = open("Generator/docs/Name.txt", "r").read().split(",")
-        print("Customer name: " + random.choice(name))
-        super().__init__()
+def build_customer():
+    customer_name = random.choice(
+        open("Generator/docs/Name.txt", "r")
+        .read()
+        .replace("\n", " ")
+        .replace("\r", "")
+        .split(",")
+    ).strip()
 
-
-class GCD:
-    def __init__(self):
-        # Get Customer Demand
-        customer_demand = (
+    customer_demand = random.choice(
+        (
             open("Generator/docs/Customer_Demand.txt", "r")
             .read()
             .replace("\n", " ")
             .replace("\r", "")
             .split(",")
         )
-        print("Customer item enquiry: " + random.choice(customer_demand))
-        super().__init__()
+    ).strip()
 
+    customer_race = random.choice(
+        open("Generator/docs/Race.txt", "r").read().split(",")
+    )
+    customer_profession = random.choice(
+        open("Generator/docs/Profession.txt", "r").read().split(",")
+    )
+    will_they_haggle = random.choice(
+        open("Generator/docs/WTH.txt", "r").read().split(",")
+    )
 
-class GP:
-    def __init__(self):
-        # Get Profession
-        profession = open("Generator/docs/Profession.txt", "r").read().split(",")
-        print("Profession: " + random.choice(profession))
-        super().__init__()
-
-
-class GR:
-    def __init__(self):
-        # Get Race
-        race = open("Generator/docs/Race.txt", "r").read().split(",")
-        print("Customer race: " + random.choice(race))
-        super().__init__()
-
-
-class WTH:
-    def __init__(self):
-        # Will They Haggle
-        haggle = open("Generator/docs/WTH.txt", "r").read().split(",")
-        print("Will they haggle? " + random.choice(haggle))
-        super().__init__()
-
-
-class Customer(GCN, GCD, GP, GR, WTH):
-    def __init__(self):
-        super().__init__()
+    return {
+        "Customer Demand": customer_demand,
+        "Customer Name": customer_name,
+        "Customer Profession": customer_profession,
+        "Customer Race": customer_race,
+        "Will They Haggle?": will_they_haggle,
+    }
