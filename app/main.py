37bf179f4a358ab5c7704b5d11d49000f1dff1fe
James Rocker <james.rocker@simplybusiness.co.uk>
2020-12-28 18:40:49 +0000

    test Procfile

--- app/main.py
+++ app/main.py
@@ -17,4 +17,7 @@ def create_app():
 
 
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    create_app = create_app()
+    create_app.run(debug=True)
+else:
+    gunicorn_app = create_app()
